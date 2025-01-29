2c4ca352f8fd19a2611acea69329ed548ab25db3
Gaurav Singh <155530336+robmark1997@users.noreply.github.com>
2025-01-29 20:32:16 +0530

    added security group and vpc

--- main.tf
+++ main.tf
@@ -1,3 +1,12 @@
+
+
+resource "aws_default_vpc" "default" {
+  tags = {
+    Name = "Default VPC"
+  }
+}
+
+
 data "aws_ami" "app_ami" {
   most_recent = true
 
@@ -21,4 +30,33 @@ resource "aws_instance" "web" {
   tags = {
     Name = "HelloWorld"
   }
+}
+
+resource "aws_security_group" "blog" {
+  name        = "blog"
+  description = "Allow TLS inbound traffic and all outbound traffic for blog instance"
+  vpc_id      = aws_vpc.default.id
+
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
+  security_group_id = aws_security_group.blog.id
+  cidr_ipv4         = aws_vpc.default.cidr_block
+  from_port         = 443
+  ip_protocol       = "https"
+  to_port           = 443
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
+  security_group_id = aws_security_group.blog.id
+  cidr_ipv4         = aws_vpc.default.cidr_block
+  from_port         = 80
+  ip_protocol       = "http"
+  to_port           = 80
+}
+
+resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
+  security_group_id = aws_security_group.blog.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1" # semantically equivalent to all ports
 }
