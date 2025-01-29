e94bff42720f9fd362c24eb55f3906841979b1a0
Gaurav Singh <155530336+robmark1997@users.noreply.github.com>
2025-01-29 21:53:42 +0530

    Update main.tf - removed old sg config

--- main.tf
+++ main.tf
@@ -44,35 +44,3 @@ module "blog-sg" {
   egress_rules            = ["all-all"]
   egress_cidr_blocks      = ["0.0.0.0/0"]
 }
-
-
-
-
-resource "aws_security_group" "blog" {
-  name        = "blog"
-  description = "Allow TLS inbound traffic and all outbound traffic for blog instance"
-  vpc_id      = data.aws_vpc.default.id
-
-}
-
-resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
-  security_group_id = aws_security_group.blog.id
-  cidr_ipv4         = data.aws_vpc.default.cidr_block
-  from_port         = 443
-  ip_protocol       = "https"
-  to_port           = 443
-}
-
-resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4-2" {
-  security_group_id = aws_security_group.blog.id
-  cidr_ipv4         = data.aws_vpc.default.cidr_block
-  from_port         = 80
-  ip_protocol       = "http"
-  to_port           = 80
-}
-
-resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
-  security_group_id = aws_security_group.blog.id
-  cidr_ipv4         = "0.0.0.0/0"
-  ip_protocol       = "-1" # semantically equivalent to all ports
-}
