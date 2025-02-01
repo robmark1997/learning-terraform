c68028c58e248f7dd96f765e863933f86d671998
Gaurav Singh <155530336+robmark1997@users.noreply.github.com>
2025-02-01 10:03:30 +0530

    removed outputs and instance rs block

--- main.tf
+++ main.tf
@@ -32,20 +32,6 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"] # Bitnami
 }
 
-/* 
-resource "aws_instance" "web" {
-  ami           = data.aws_ami.app_ami.id
-  instance_type = var.instance_type
-
-  subnet_id = module.blog_vpc.public_subnets[0]
-
-  vpc_security_group_ids = [module.blog-sg.security_group_id]
-
-  tags = {
-    Name = "HelloWorld"
-  }
-} 
-*/
 
 
 module "autoscaling" {
